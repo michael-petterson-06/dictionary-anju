91020259197366e85fbf63a9bee053679fb89544
Michael <michaelpetterson06.com.br>
2023-11-15 20:59:54 -0300

    header-input

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import bookSvg from './images/book.svg';
+
+export default function NavBar() {
+
+  return (
+
+    <header className="h-20 flex items-center">
+      <Image
+         src={ bookSvg }
+         width='1000'
+         height='1000'
+         alt='book svg'
+         className='w-10'
+      />
+    </header>
+  )
+
+}
